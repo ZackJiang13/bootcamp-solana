4a2c43f077e404ca2e3a4361edc3e63ef61b5ce8
Zack Jiang <zackjiang11@gmail.com>
2025-05-31 23:45:11 +0400

    Refactor voting initialization: rename function to initialize_pool and implement InitializePoll struct with Poll account definition.

--- programs/bootcamp-voting/src/lib.rs
+++ programs/bootcamp-voting/src/lib.rs
@@ -6,11 +6,35 @@ declare_id!("iAADC1NuKH31rgthtsKMChE1KHG1QTigB8uYmySRRT7");
 pub mod bootcamp_voting {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_pool(ctx: Context<InitializePoll>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(poll_id: u64)]
+pub struct InitializePoll<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + Poll::INIT_SPACE,
+        seeds = [poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll: Account<'info, Poll>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Poll {
+    pub poll_id: u64,
+    #[max_len(280)]
+    pub description: String,
+    pub poll_start: u64,
+    pub poll_end: u64,
+    pub candidate_amount: u64,
+}
