741efac424b246cef39977065a63fd43ff827308
Zack Jiang <zackjiang11@gmail.com>
2025-06-04 22:43:48 +0400

    Add CRUD program with journal entry functionality, including Cargo.toml, Xargo.toml, and initial implementation in lib.rs.

---
+++ programs/crud/src/lib.rs
@@ -0,0 +1,43 @@
+use anchor_lang::prelude::*;
+
+declare_id!("iAADC1NuKH31rgthtsKMChE1KHG1QTigB8uYmySRRT7");
+
+#[program]
+pub mod crud {
+    use super::*;
+
+    pub fn create_journal_entry(ctx: Context<CreateJournalEntry>, title: String, content: String) -> Result<()> {
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.owner = ctx.accounts.signer.key();
+        journal_entry.title = title;
+        journal_entry.message = content;
+        msg!("Journal entry created successfully");
+        Ok(())
+    }
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct JournalEntryState{
+    pub owner: Pubkey,
+    #[max_len(32)]
+    pub title: String,
+    #[max_len(280)]
+    pub message: String,
+}
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct CreateJournalEntry<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init, 
+        payer = signer, 
+        space = 8 + JournalEntryState::INIT_SPACE,
+        seeds = [title.as_bytes().as_ref(), signer.key().as_ref()],
+        bump
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+    pub system_program: Program<'info, System>,
+}
